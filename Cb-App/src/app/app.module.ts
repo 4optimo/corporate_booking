bc293f549ba571066d9200da3d256dec335bf7c4
Dinesh <djay23.work@gmail.com>
2018-04-09 18:47:33 +0530

    Testing push - Dinesh

--- Cb-App/src/app/app.module.ts
+++ Cb-App/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { AlertsComponent } from './alerts/alerts.component';
+import { BookingSummaryComponent } from './booking-summary/booking-summary.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    AlertsComponent
+    AlertsComponent,
+    BookingSummaryComponent
   ],
   imports: [
     BrowserModule
